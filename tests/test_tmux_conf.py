09446d08054d5c2fbd08773e62e087eab72ec4e9
jaclu <jacob.lundqvist@gmail.com>
2022-11-08 17:48:36 +0100

    Added test for plugins found

--- tests/test_tmux_conf.py
+++ tests/test_tmux_conf.py
@@ -1,11 +1,34 @@
+from common_vars import CONF_FILE
+
 from tmux_conf.tmux_conf import TmuxConfig
 from tmux_conf.utils import run_shell
 
-CONF_FILE = "/tmp/tmux_test/tmux.conf"
-
 BIND_CMD = "bind -N 'This is a note'  a  display 'that was a'"
-
 CONTENT_LINE = "# Hello world"
+PLUGIN_SOURCE = "jaclu"
+PLUGIN_NAME = "tmux-prefix-highlight"
+
+
+class PluginsSample(TmuxConfig):
+    def content(self):
+        self.write("# dummy content")
+
+    def plugin_prefix_highlight(self):
+        #
+        #  Highlights when you press tmux prefix key and
+        #  when copy/sync mode is active.
+        #
+        conf = """
+        set -g @prefix_highlight_show_copy_mode  on
+        set -g @prefix_highlight_copy_mode_attr  "fg=black,bg=yellow,bold"
+        set -g @prefix_highlight_show_sync_mode  on
+        set -g @prefix_highlight_sync_mode_attr "fg=black,bg=orange,blink,bold"
+        """
+        return [f"{PLUGIN_SOURCE}/{PLUGIN_NAME}", 2.4, conf]
+
+
+class PluginsDisabled(PluginsSample):
+    plugin_handler = ""
 
 
 class NotEmbedded(TmuxConfig):
@@ -17,6 +40,12 @@ class NotEmbedded(TmuxConfig):
 
     def content(self):
         self.write(CONTENT_LINE)
+
+
+def prep_plugin_class(cls, version):
+    ps = cls(parse_cmd_line=False, conf_file=CONF_FILE, tmux_version=version)
+    ps.plugins.scan(ps.list_plugin_methods())
+    return ps
 
 
 def tc_env():
@@ -53,7 +82,7 @@ def test_tc_is_not_tmate():
 def test_tc_is_tmate():
     tc = tc_env()
     #  This assumes there is a tmate in PATH
-    if not (tmate_cmd := run_shell('command -v tmate')):
+    if not (tmate_cmd := run_shell("command -v tmate")):
         return  # Can't do this test
     tc.use_tmux_bin(tmate_cmd)
     assert tc.is_tmate() is True
@@ -130,3 +159,21 @@ def test_tc_conf_file_header_and_content():
 
     assert edit_conf_found is True
     tc.remove_conf_file()
+
+
+def test_tc_plugin_found():
+    ps = prep_plugin_class(cls=PluginsSample, version=2.4)
+    assert ps.plugins.found() == [PLUGIN_NAME]
+    assert ps.plugins.found(short_name=False) == [f"{PLUGIN_SOURCE}/{PLUGIN_NAME}"]
+
+
+def test_tc_plugin_unavailable():
+    ps = prep_plugin_class(cls=PluginsSample, version=2.0)
+    assert not ps.plugins.found()
+    assert not ps.plugins.found(short_name=False)
+
+
+def test_tc_plugins_disabled():
+    ps = prep_plugin_class(cls=PluginsDisabled, version=2.4)
+    assert not ps.plugins.found()
+    assert not ps.plugins.found(short_name=False)
