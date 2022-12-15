be993f065cba09c08c56ffe3f9c422f3d8532bb2
jaclu <jacob.lundqvist@gmail.com>
2022-12-15 20:05:59 +0100

    debug of test stuff

---
+++ tests/debug_it.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Sample config, showing how to use settings depending on a recent
+tmux version. And defining some plugins of various complexity
+
+Also shows various methods to define plugins
+"""
+from tmux_conf.tmux_conf import TmuxConfig
+from utils import CONF_FILE
+
+# from local_tmux_conf.tests.utils import CONF_FILE
+
+
+class WithContent(TmuxConfig):
+    def content(self):
+        self.write("bind -N")
+
+
+#  First clear all plugins installed
+WithContent(
+    parse_cmd_line=False, conf_file=CONF_FILE, replace_config=True, tmux_version=3.0
+).run()
